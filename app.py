44b66fec63de82e2feb467420a6e23107cd6c9b8
eshap <eshapiro@eshap.tv>
2026-07-03 18:26:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ st.markdown(
 )
 
 # Permanent Link Markdown Lock: Native un-sanitized notation forcing absolute target subdomain security
-st.markdown("We will update this index monthly, on a rolling six months basis.<br>Silultaneously, we will drop analysis of the latest data on **[Media War & Peace](https://substack.com)**.", unsafe_allow_html=True)
+st.markdown("We will update this index monthly, on a rolling six months basis.<br>Each month, we will drop our analysis of the latest data on **[Media War & Peace](https://substack.com)**.", unsafe_allow_html=True)
 
 st.html("<style>div[data-testid='stSidebarNav'] + div, div[data-testid='stRadio'] > div { gap: 0.25rem !important; padding: 0 !important; } div[data-testid='stRadio'] label p { font-size: 0.88rem !important; margin: 0 !important; }</style>")
 
